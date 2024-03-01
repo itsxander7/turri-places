fc758e6d94a2b0b1355db226a396951365101708
Alexander Alvarez <125148489+itsxander7@users.noreply.github.com>
2024-03-01 00:58:15 -0600

    Application in progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Hola</h1>);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1>TurriPlaces</h1>
+);
